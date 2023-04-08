9fc0a88b5948710c9309307cb97cd9e6bd383870
George Williams <george.williams@gmail.com>
2023-04-07 17:11:47 -0700

    fixing dict err

--- gsi/docker/bench2v/benchmark_client_import.py
+++ gsi/docker/bench2v/benchmark_client_import.py
@@ -213,7 +213,7 @@ else:
         sleep(5)
     elif VECTOR_INDEX=="hnsw" and not ALLOW_CACHEING:
         print("Hnsw parameter check: got", cls_schema['vectorIndexConfig'])
-        if cls_schema['vectorIndexConfig']["vectorIndexConfig"]!= 0:
+        if cls_schema['vectorIndexConfig']["vectorCacheMaxObjects"]!= 0:
             raise Exception("Invalid hnsw index config")
         print("Hnsw config ok.")
         sleep(5)
