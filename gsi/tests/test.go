66335b101b2605ee3b16b25dc9b7e880a90d19ed
George Williams <george.williams@gmail.com>
2023-04-11 23:12:15 +0000

    worked around bug in fvs/append_float32 call when file already exists

--- gsi/tests/test.go
+++ gsi/tests/test.go
@@ -3,23 +3,26 @@ package main
 
 import (
     "fmt"
-    "os"
-    "log"
-    "io/ioutil"
-    //"testing"
+    "time"
+    "math/rand"
 
     geminiplugin "github.com/gsi/weaviate/gemini_plugin"
 )
 
+// Generate a random string useful for generate a temp filename that does not already exist
+func randomString(length int) string {
+    rand.Seed(time.Now().UnixNano())
+    b := make([]byte, length+2)
+    rand.Read(b)
+    return fmt.Sprintf("%x", b)[2 : length+2]
+}
+
 func main() {
 
     // Create a temp file that will automatically delete itself when this app is done
-    file, err := ioutil.TempFile("/tmp/", "gemini_plugin_test_")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer os.Remove(file.Name())
-    fmt.Println("Got temp file->", file.Name())
+    ranstr := randomString(10)
+    ranfilepath := fmt.Sprintf("/tmp/gemini_plugin_test_%s", ranstr)
+    fmt.Println("Got temp file path->", ranfilepath)
 
     //
     // Create a float array of dims = 96
@@ -32,7 +35,8 @@ func main() {
     // Build a numpy file with the array
     //
     fmt.Println("Testing Numpy_append_float32_array")
-    row_count, dim, aerr := geminiplugin.Numpy_append_float32_array( file.Name(), arr, 96, 1)
+    //row_count, dim, aerr := geminiplugin.Numpy_append_float32_array( file.Name(), arr, 96, 1)
+    row_count, dim, aerr := geminiplugin.Numpy_append_float32_array( ranfilepath, arr, 96, 1)
     if aerr != nil {
         fmt.Println("Error creating numpy file with float32 array.", aerr)
     } else {
