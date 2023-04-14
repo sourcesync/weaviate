6ae5075094a97c92a0d9142db7d13878ce53af90
George Williams <george.williams@gmail.com>
2023-04-13 18:17:00 -0700

    reverting search-time-as-unix-time hack to pass cicd

--- adapters/repos/db/refcache/resolver.go
+++ adapters/repos/db/refcache/resolver.go
@@ -193,9 +193,10 @@ func (r *Resolver) resolveRef(item *models.SingleRef, desiredClass string,
 		nested["creationTimeUnix"] = res.Created
 	}
 	if additionalProperties.LastUpdateTimeUnix {
-		// This is a horrible hack
+		// This is a horrible hack.  Chooose from
 		// nested["lastUpdateTimeUnix"] = res.Updated
-		nested["lastUpdateTimeUnix"] = res.SearchTime
+		// nested["lastUpdateTimeUnix"] = res.SearchTime
+		nested["lastUpdateTimeUnix"] = res.Updated
 	}
 	out.Fields = nested
 
