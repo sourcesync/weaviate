4111704d6d864c7ec8a02f56f1316922f3542faa
George Williams <george.williams@gmail.com>
2023-04-25 17:37:31 +0000

    new flat run scripts with more http logging

--- gsi/fvs/run_benchmarks_deep2M_q10.sh
+++ gsi/fvs/run_benchmarks_deep2M_q10.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+set -x
+
 #
 # You need to change these parameters to reflect your local setup
 #
 
 # Write/append the benchmark results to this file
-OUTPUT="results/benchmarks-deep2M-q10.csv"
+DT=$(date +%s)
+DTDIR="results/$DT"
+mkdir -p $DTDIR
+OUTPUT="$DTDIR/benchmarks-deep2M-q10-$DT.csv"
 
 # Set a valid allocation id
-ALLOCATION_ID="fd283b38-3e4a-11eb-a205-7085c2c5e516"
+ALLOCATION_ID="0b391a1a-b916-11ed-afcb-0242ac1c0002" #"fd283b38-3e4a-11eb-a205-7085c2c5e516"
 
 # Path to dataset numpy file
 DATASET="/mnt/nas1/fvs_benchmark_datasets/deep-2M.npy"
@@ -16,8 +22,11 @@ DATASET="/mnt/nas1/fvs_benchmark_datasets/deep-2M.npy"
 # Path to queries numpy file
 QUERIES="/mnt/nas1/fvs_benchmark_datasets/deep-queries-10.npy"
 
-## Path to the ground truth numpy file
+# Path to the ground truth numpy file
 GROUNDTRUTH="/mnt/nas1/fvs_benchmark_datasets/deep-2M-gt-10.npy"
+
+# Set Swagger to emit verbose http payload messages
+export GEMINI_SWAGGER_VERBOSE='true'
 
 #
 # Check the arguments and the system before running the benchmarks.
@@ -33,20 +42,21 @@ set -e
 #
 # Now run all the benchmarks.  Note that this might take a while, so you should consider running it behind the 'screen' utility.
 #
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/2M_10_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/2M_10_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/2M_10_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/2M_10_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/2M_10_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/2M_10_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/2M_10_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/2M_10_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/2M_10_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/2M_10_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/2M_10_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/2M_10_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64 2>&1 | tee "$DTDIR/2M_10_64b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64 2>&1 | tee "$DTDIR/2M_10_64b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64 2>&1 | tee "$DTDIR/2M_10_64b_$DT_$(date +%s).txt"
 
+echo "Done. $OUTPUT"
 
--- gsi/fvs/run_benchmarks_deep5M_q1000.sh
+++ gsi/fvs/run_benchmarks_deep5M_q1000.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+set -x
+
 #
 # You need to change these parameters to reflect your local setup
 #
 
 # Write/append the benchmark results to this file
-OUTPUT="results/benchmarks-deep5M-q1000.csv"
+DT=$(date +%s)
+DTDIR="results/$DT"
+mkdir -p $DTDIR
+OUTPUT="$DTDIR/benchmarks-deep5M-q1000-$DT.csv"
 
 # Set a valid allocation id
-ALLOCATION_ID="fd283b38-3e4a-11eb-a205-7085c2c5e516"
+ALLOCATION_ID="0b391a1a-b916-11ed-afcb-0242ac1c0002" #"fd283b38-3e4a-11eb-a205-7085c2c5e516"
 
 # Path to dataset numpy file
 DATASET="/mnt/nas1/fvs_benchmark_datasets/deep-5M.npy"
@@ -16,8 +22,11 @@ DATASET="/mnt/nas1/fvs_benchmark_datasets/deep-5M.npy"
 # Path to queries numpy file
 QUERIES="/mnt/nas1/fvs_benchmark_datasets/deep-queries-1000.npy"
 
-## Path to the ground truth numpy file
+# Path to the ground truth numpy file
 GROUNDTRUTH="/mnt/nas1/fvs_benchmark_datasets/deep-5M-gt-1000.npy"
+
+# Set Swagger to emit verbose http payload messages
+export GEMINI_SWAGGER_VERBOSE='true'
 
 #
 # Check the arguments and the system before running the benchmarks.
@@ -33,20 +42,21 @@ set -e
 #
 # Now run all the benchmarks.  Note that this might take a while, so you should consider running it behind the 'screen' utility.
 #
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
-python gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/5M_1000_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/5M_1000_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 768 2>&1 | tee "$DTDIR/5M_1000_768b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/5M_1000_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/5M_1000_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 512 2>&1 | tee "$DTDIR/5M_1000_512b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/5M_1000_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/5M_1000_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 256 2>&1 | tee "$DTDIR/5M_1000_256b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/5M_1000_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/5M_1000_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 128 2>&1 | tee "$DTDIR/5M_1000_128b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64  2>&1 | tee "$DTDIR/5M_1000_64b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64  2>&1 | tee "$DTDIR/5M_1000_64b_$DT_$(date +%s).txt"
+python -u gemini_fvs.py -a "$ALLOCATION_ID" -d "$DATASET" -q "$QUERIES" -g "$GROUNDTRUTH"  -o "$OUTPUT" --b 64  2>&1 | tee "$DTDIR/5M_1000_64b_$DT_$(date +%s).txt"
 
+echo "Done. $OUTPUT"
 
