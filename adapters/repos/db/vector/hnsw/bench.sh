4029b54169e5ec40760e0149424040f730577b33
Jacob Benson <jbenson116@gmail.com>
2023-05-27 14:35:21 -0700

    changes for multi-benchmark index persistence

--- adapters/repos/db/vector/hnsw/bench.sh
+++ adapters/repos/db/vector/hnsw/bench.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-export DATANAME=10K
-export DATASIZE=10000
+export DATASIZE=500000000
 export QUERYSIZE=1000
+export INCREMENT=50000000
+export START=100000000
 
 go test -run TestBench -v -timeout 0
--- adapters/repos/db/vector/hnsw/bench_test.go
+++ adapters/repos/db/vector/hnsw/bench_test.go
@@ -18,7 +18,13 @@ import (
 
 const (
 	datadir = "/mnt/nas1/fvs_benchmark_datasets"
-	csvpath = "/mnt/nas1/weaviate_benchmark_results/algo_direct/"
+	// csvpath = "/mnt/nas1/weaviate_benchmark_results/algo_direct/"
+	csvpath      = "/home/jacob/"
+	multi        = true
+	increment, _ = strconv.Atoi(os.Getenv("INCREMENT"))
+	k            = 10
+	dims         = 96
+	gt_size      = 100
 )
 
 func fileExists(fname string) bool {
@@ -29,7 +35,7 @@ func fileExists(fname string) bool {
 	return !info.IsDir()
 }
 
-func WriteToCSV(n uint, q uint, k int, ef int, loadTime float64, searchTime float64, avgRecall float32, t1 time.Time, t2 time.Time) {
+func WriteToCSV(data_name string, n uint, q uint, k int, ef int, loadTime float64, searchTime float64, avgRecall float32, t1 time.Time, t2 time.Time) {
 	server, _ := os.Hostname()
 	fname := fmt.Sprintf("%s%s_%s.csv", csvpath, server, data_name)
 	if fileExists(fname) {
@@ -127,21 +133,63 @@ func ReadInt32(path string, shape []uint) [][]int32 {
 	return data
 }
 
+func run_queries(queryVectors [][]float32, gt [][]int32, index *hnsw, k int, ef int) float32 {
+	var recalls []float32
+	var ys, ns, total int = 0, 0, 0
+	for i, vec := range queryVectors {
+		res, _, err := index.knnSearchByVector(vec, int(k), ef, nil)
+		if err != nil {
+			panic(err)
+		}
+		var y, n int = 0, 0
+		total += len(res)
+
+		if i == 0 {
+			fmt.Println("length of res:", len(res), " res[0]:", res[0])
+		}
+		for j := range res {
+			if gt[i][j] == int32(res[j]) {
+				y += 1
+			} else {
+				n += 1
+			}
+		}
+		ys += y
+		ns += n
+		recalls = append(recalls, float32(y)/float32(len(res)))
+	}
+	var sum float32
+	for _, recall := range recalls {
+		sum += recall
+	}
+	avg_recall := sum / float32(len(recalls))
+	fmt.Println("ys:", ys, "  ns:", ns, "  avg recall:", avg_recall)
+	return avg_recall
+}
+
 var (
-	data_name     = os.Getenv("DATANAME")
 	data_size, _  = strconv.Atoi(os.Getenv("DATASIZE"))
 	query_size, _ = strconv.Atoi(os.Getenv("QUERYSIZE"))
+	start_size, _ = strconv.Atoi(os.Getenv("START"))
 )
 
 func TestBench(t *testing.T) {
-	var dims, gt_size uint = 96, 100
+	tmp := len(fmt.Sprintf("%d", data_size))
+	var data_name string
+	if tmp < 7 { // if data size is in the thousands name is size/1000 + K
+		data_name = fmt.Sprintf("%dK", data_size/1000)
+	} else if tmp < 10 { // if data size is in millions name is size/1000000 + M
+		data_name = fmt.Sprintf("%dM", data_size/1000000)
+	}
+
 	fmt.Println("Benchmark Test:", data_name, data_size, query_size, "start time:", time.Now().Format("2006-01-02 15:04:05"))
+
 	var data_size, query_size = uint(data_size), uint(query_size)
 	data_path := fmt.Sprintf("%s/deep-%s.npy", datadir, data_name)
 	query_path := fmt.Sprintf("%s/deep-queries-%d.npy", datadir, query_size)
 	gt_path := fmt.Sprintf("%s/deep-%s-gt-%d.npy", datadir, data_name, query_size)
 	paths := []string{data_path, query_path, gt_path}
-	for _, path := range paths {
+	for _, path := range paths { // check files exist
 		_, err := os.Stat(path)
 		if err != nil {
 			panic(err)
@@ -153,10 +201,11 @@ func TestBench(t *testing.T) {
 	queryVectors := ReadFloat32(query_path, []uint{query_size, dims})
 	gt := ReadInt32(gt_path, []uint{query_size, gt_size})
 
+	// assertions for vector shape
 	assert.Equal(t, int(data_size), len(testVectors))
 	assert.Equal(t, int(query_size), len(queryVectors))
-	assert.Equal(t, int(query_size), len(gt))
 
+	// initialize hnsw index
 	makeCL := MakeNoopCommitLogger
 	vectorFunc := func(ctx context.Context, id uint64) ([]float32, error) {
 		return testVectors[int(id)], nil
@@ -178,53 +227,37 @@ func TestBench(t *testing.T) {
 		Distance:         "cos",
 	})
 	require.Nil(t, err)
+	ef := index.autoEfFromK(int(k))
 
-	fmt.Println("loading vectors...")
-	t1 := time.Now()
-	for i, vec := range testVectors {
+	for i, vec := range testVectors[:start_size] {
 		err := index.Add(uint64(i), vec)
 		require.Nil(t, err)
 	}
 
-	var k int = 10
-	ef := index.autoEfFromK(int(k))
+	// loop for queries, break after 1 iteration if "multi" is false
+	for i := 0; i < 5; i++ {
+		// read groundtruth from current data size
+		gt_path := fmt.Sprintf("%s/deep-%s-gt-%d.npy", datadir, data_name, query_size)
+		gt := ReadInt32(gt_path, []uint{query_size, gt_size})
+		assert.Equal(t, int(query_size), len(gt))
 
-	load_time := time.Since(t1).Seconds()
-	fmt.Println("load time:", load_time, " seconds")
-
-	t2 := time.Now()
-	fmt.Println("running queries...")
-	var ys, ns, total int = 0, 0, 0
-	var recalls []float32
-
-	for i, vec := range queryVectors {
-		res, _, err := index.knnSearchByVector(vec, int(k), ef, nil)
-		var y, n int = 0, 0
-		total += len(res)
-		assert.Nil(t, err)
-		if i == 0 {
-			fmt.Println("length of res:", len(res), " res[0]:", res[0])
+		fmt.Println("loading vectors...")
+		t1 := time.Now()
+		for j, vec := range testVectors[increment*i : increment*(i+1)] {
+			err := index.Add(uint64(j), vec)
+			require.Nil(t, err)
 		}
-		for j := range res {
-			if gt[i][j] == int32(res[j]) {
-				y += 1
-			} else {
-				n += 1
-			}
-		}
-		ys += y
-		ns += n
-		recalls = append(recalls, float32(y)/float32(len(res)))
-	}
-	var sum float32
-	for _, recall := range recalls {
-		sum += recall
-	}
-	avg_recall := sum / float32(len(recalls))
-	fmt.Println("ys:", ys, "  ns:", ns, "  avg recall:", avg_recall)
-	search_time := time.Since(t2).Seconds()
-	fmt.Println("search time:", search_time, " seconds")
+		fmt.Println(i)
 
-	WriteToCSV(data_size, query_size, k, ef, load_time, search_time, avg_recall, t1, t2)
+		load_time := time.Since(t1).Seconds()
+		fmt.Println("load time:", load_time, " seconds")
+
+		t2 := time.Now()
+		fmt.Println("running queries...")
+		avg_recall := run_queries(queryVectors, gt[increment*i:increment*i+1], index, k, ef)
+		search_time := time.Since(t2).Seconds()
+		fmt.Println("search time:", search_time, " seconds")
+		WriteToCSV(data_name, data_size, query_size, k, ef, load_time, search_time, avg_recall, t1, t2)
+	}
 	fmt.Println("Done.")
 }
