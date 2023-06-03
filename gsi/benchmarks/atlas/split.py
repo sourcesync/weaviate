cd5850d3ca994c31cabfb43c0f4fb30992c4d22a
iamsabhoho <sabrina.sj.ho@gmail.com>
2023-06-03 00:20:57 +0000

    finished split

--- gsi/benchmarks/atlas/split.py
+++ gsi/benchmarks/atlas/split.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import random
+from tqdm import tqdm
 
 # CONFIG
 ATLAS_EMBEDDINGS = "/mnt/nas1/atlas_data/benchmarking/atlas.npy"
@@ -32,9 +33,19 @@ arr_lst = []
 for idx in LST_IND:
     q_arr = arr[idx]#.reshape(1, 768)
     arr_lst.append(q_arr)
-    print("deleting ", idx, " from base atlas...")
-    arr_new = np.delete(arr_cp, idx)
+    #print("deleting ", idx, " from base atlas...")
+    #arr_new = np.delete(arr_cp, idx)
 
 # save 1000 query vectors
 np.save(QUERY_VECS, arr_lst)
+
+# create base atlas
+arr_new = []
+
+for i in tqdm(range(TOTAL_LENGTH)):
+    if i not in LST_IND:
+        #arr_new = np.take(arr_cp, i)
+        arr_new.append(arr[i])
+
+print("saving base embeddings now.. ")
 np.save(BASE_ATLAS, arr_new)
