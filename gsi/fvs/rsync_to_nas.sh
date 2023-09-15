a45c01a29ca56e8cf3b77f0a1e4d5ac51239030d
George Williams <george.williams@gmail.com>
2023-09-15 10:29:12 -0700

    changing name of dated target sync dir

--- gsi/fvs/rsync_to_nas.sh
+++ gsi/fvs/rsync_to_nas.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-TARGET="gwilliams@192.168.99.107:/mnt/nas1/weaviate_benchmark_results/fvs/$(hostname)/all_09092023/"
+TARGET="gwilliams@192.168.99.107:/mnt/nas1/weaviate_benchmark_results/fvs/$(hostname)/all_09142023/"
 
 rsync -azvdO results/qbq "$TARGET"
