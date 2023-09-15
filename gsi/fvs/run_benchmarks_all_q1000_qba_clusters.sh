40a69f777bb9756c18a1cd16d17206e8b795dae0
George Williams <george.williams@gmail.com>
2023-09-15 10:20:12 -0700

    now running 100M

--- gsi/fvs/run_benchmarks_all_q1000_qba_clusters.sh
+++ gsi/fvs/run_benchmarks_all_q1000_qba_clusters.sh
@@ -3,12 +3,12 @@
 set -x
 set -e
 
-./run_benchmarks_deep1M_q1000_qbq_clusters.sh
-./run_benchmarks_deep2M_q1000_qbq_clusters.sh
-./run_benchmarks_deep5M_q1000_qbq_clusters.sh
-./run_benchmarks_deep10M_q1000_qbq_clusters.sh
-./run_benchmarks_deep20M_q1000_qbq_clusters.sh
-./run_benchmarks_deep50M_q1000_qbq_clusters.sh
-#./run_benchmarks_deep100M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep1M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep2M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep5M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep10M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep20M_q1000_qbq_clusters.sh
+#./run_benchmarks_deep50M_q1000_qbq_clusters.sh
+./run_benchmarks_deep100M_q1000_qbq_clusters.sh
 #./run_benchmarks_deep250M_q1000_qbq_clusters.sh
 #./run_benchmarks_deep500M_q1000_qbq_clusters.sh
